0429afcfcd505eef2d577007b314ddf46c3814a6
gabriel <gabart4410@gmail.com>
2026-02-28 23:55:31 -0300

    elenco e patrocinadores

--- hawksite/core/models.py
+++ hawksite/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Player(models.Model):
+    nome = models.CharField(max_length=100)
+    numero = models.IntegerField()
+    posicao = models.CharField(max_length=50)
+    foto = models.ImageField(upload_to='players/')
+
+    def __str__(self):
+        return f"{self.numero} - {self.nome}"
--- hawksite/core/views.py
+++ hawksite/core/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Player
 
 def home(request):
     return render(request, 'core/home.html')
+
+def elenco(request):
+    jogadores = Player.objects.all()
+    return render(request, 'core/elenco.html', {'jogadores': jogadores})
